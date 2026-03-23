cd737b499f53d02d858f63d42cbc97abb43f44f7
cen1 <cen.is.imba@gmail.com>
2026-03-23 21:22:57 +0100

    remove ms_stdint.h, only needed pre msvc 2010

--- src/bncsutil/mutil_types.h
+++ src/bncsutil/mutil_types.h
@@ -26,14 +26,10 @@
 #ifndef BNCSUTIL_MUTIL_TYPES_H_INCLUDED
 #define BNCSUTIL_MUTIL_TYPES_H_INCLUDED
 
-#if defined(_MSC_VER) && (defined(HAVE_STDINT_H) && !HAVE_STDINT_H)
- #include "ms_stdint.h"
-#else
 #ifdef __cplusplus
 #include <cstdint>
 #else
 #include <stdint.h>
-#endif
 #endif
 
 #endif /* BNCSUTIL_MUTIL_TYPES_H_INCLUDED */
